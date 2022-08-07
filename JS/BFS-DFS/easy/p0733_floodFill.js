5277f2aa6acc05b5baa800150bb29e885b74331f
nilswg <nilsonweng@gmail.com>
2022-08-08 02:00:42 +0800

    p0733 補說明與測試

--- JS/BFS-DFS/easy/p0733_floodFill.js
+++ JS/BFS-DFS/easy/p0733_floodFill.js
@@ -6,58 +6,39 @@ import { isArrayEqual } from '../bfs-dfs.js';
 // https://leetcode.com/problems/flood-fill/
 
 // 題目說明
+// An image is represented by an m x n integer grid image where image[i][j] represents the pixel value of the image.
+//
+// You are also given three integers sr, sc, and color. You should perform a flood fill on the image starting from the pixel image[sr][sc].
+//
+// To perform a flood fill, consider the starting pixel, plus any pixels connected 4-directionally
+// to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally
+// to those pixels (also with the same color), and so on. Replace the color of all of the aforementioned pixels with color.
+//
+// Return the modified image after performing the flood fill.
+/**
+ * 假如從 [1,1] 的位置開始, 跟改為 2
+ *
+ * Example 1:
+ *
+ *   [1, 1, 0],      [2, 2, 0]
+ *   [1, 1, 0],  ->  [2, 2, 0]
+ *   [1, 0, 1],      [2, 0, 1]
+ *
+ * Example 2:
+ *
+ * 假如從 [1,1] 的位置開始, 跟改為 0
+ *
+ *   [0, 0, 0],      [0, 0, 0]
+ *   [0, 0, 0],  ->  [0, 0, 0]
+ *   (因為起始顏色跟新填入的顏色相同，所以直接返回)
+ */
 
 // 解題重點
+// 1.瞭解BFS、DFS
+// 2.解題前應能思考出此題更適合用DFS來實作
+// 3.注意，當color與 image[sr][sc] 相同時，應該立即返回，不用處理。
 
 // 解題思路
-
-// 複雜度
-// Time Complexity : O(??)
-// Space Complexity: O(??)
-
-/**
- * @param {number[][]} image
- * @param {number} sr
- * @param {number} sc
- * @param {number} color
- * @return {number[][]}
- */
-var floodFill = function (image, sr, sc, color) {
-  let m = image.length;
-  let n = image[0].length;
-
-  let search = (i, j, k) => {
-    // 超出範圍
-    if (i < 0 || j < 0 || i >= m || j >= n) return false;
-
-    // 搜尋對象
-    if (image[i][j] !== k) return false;
-
-    // 暫存狀態
-    const temp = image[i][j];
-
-    // 帶換成無效值，避免被重複設值
-    image[i][j] = -1;
-
-    // 繼續搜尋
-    const found =
-      search(i - 1, j, k) ||
-      search(i + 1, j, k) ||
-      search(i, j - 1, k) ||
-      search(i, j + 1, k);
-
-    // 設置新值
-    if (!found) {
-      image[i][j] = color;
-    }
-
-    return found; // 是否繼續搜尋。
-  };
-
-  search(sr, sc, image[sr][sc]);
-
-  return image;
-};
 
 // BFS
 //
@@ -73,6 +54,11 @@ var floodFill = function (image, sr, sc, color) {
  * @return {number[][]}
  */
 var floodFillBFS = function (image, sr, sc, color) {
+  // 當進入點已是color時，則不處理。
+  if (image[sr][sc] === color) {
+    return image;
+  }
+
   let quene = [[sr, sc]];
   let m = image.length;
   let n = image[0].length;
@@ -115,6 +101,11 @@ var floodFillBFS = function (image, sr, sc, color) {
  * @return {number[][]}
  */
 var floodFillDFS = function (image, sr, sc, color) {
+  // 當進入點已是color時，則不處理。
+  if (image[sr][sc] === color) {
+    return image;
+  }
+
   let m = image.length;
   let n = image[0].length;
   let originalColor = image[sr][sc];
@@ -134,11 +125,58 @@ var floodFillDFS = function (image, sr, sc, color) {
     helper(r - 1, c);
     helper(r, c + 1);
     helper(r, c - 1);
-
-    //return;
   };
 
   helper(sr, sc);
+  return image;
+};
+
+// 自我延伸練習: DFS-BackTracking
+//
+// 複雜度
+// Time Complexity : O(N)
+// Space Complexity: O(N)
+
+/**
+ * @param {number[][]} image
+ * @param {number} sr
+ * @param {number} sc
+ * @param {number} color
+ * @return {number[][]}
+ */
+var floodFillBackTracking = function (image, sr, sc, color) {
+  let m = image.length;
+  let n = image[0].length;
+  let originalColor = image[sr][sc];
+
+  let search = (i, j) => {
+    // 超出範圍
+    if (i < 0 || j < 0 || i >= m || j >= n) return false;
+
+    // 搜尋對象
+    if (image[i][j] !== originalColor) return false;
+
+    // 暫存狀態，帶換成無效值，避免被重複設值
+    // const temp = image[i][j];
+    image[i][j] = -1;
+
+    // 繼續搜尋
+    const found =
+      search(i - 1, j) ||
+      search(i + 1, j) ||
+      search(i, j - 1) ||
+      search(i, j + 1);
+
+    // 設置新值
+    if (!found) {
+      image[i][j] = color;
+    }
+
+    return found; // 是否繼續搜尋。
+  };
+
+  search(sr, sc);
+
   return image;
 };
 
@@ -146,9 +184,55 @@ var floodFillDFS = function (image, sr, sc, color) {
 (function () {
   console.log('Testing [p0079_floodFill]...');
 
+  console.log('Testing [floodFillBFS]...');
+
   console.log(
     isArrayEqual(
       floodFillBFS(
+        [
+          [1, 1, 0],
+          [1, 1, 0],
+          [1, 0, 1],
+        ],
+        1,
+        1,
+        2
+      ),
+      [
+        [2, 2, 0],
+        [2, 2, 0],
+        [2, 0, 1],
+      ]
+    ) === true
+  );
+  [
+    [0, 0, 0],
+    [0, 0, 0],
+  ];
+
+  console.log(
+    isArrayEqual(
+      floodFillBFS(
+        [
+          [0, 0, 0],
+          [0, 0, 0],
+        ],
+        0,
+        0,
+        0
+      ),
+      [
+        [0, 0, 0],
+        [0, 0, 0],
+      ]
+    ) === true
+  );
+
+  console.log('Testing [floodFillDFS]...');
+
+  console.log(
+    isArrayEqual(
+      floodFillDFS(
         [
           [1, 1, 0],
           [1, 1, 0],
@@ -170,6 +254,26 @@ var floodFillDFS = function (image, sr, sc, color) {
     isArrayEqual(
       floodFillDFS(
         [
+          [0, 0, 0],
+          [0, 0, 0],
+        ],
+        0,
+        0,
+        0
+      ),
+      [
+        [0, 0, 0],
+        [0, 0, 0],
+      ]
+    ) === true
+  );
+
+  console.log('Testing [floodFillBackTracking]...');
+
+  console.log(
+    isArrayEqual(
+      floodFillBackTracking(
+        [
           [1, 1, 0],
           [1, 1, 0],
           [1, 0, 1],
@@ -182,6 +286,24 @@ var floodFillDFS = function (image, sr, sc, color) {
         [2, 2, 0],
         [2, 2, 0],
         [2, 0, 1],
+      ]
+    ) === true
+  );
+
+  console.log(
+    isArrayEqual(
+      floodFillBackTracking(
+        [
+          [0, 0, 0],
+          [0, 0, 0],
+        ],
+        0,
+        0,
+        0
+      ),
+      [
+        [0, 0, 0],
+        [0, 0, 0],
       ]
     ) === true
   );
