aab43bb099146a2a3849aeaf250d624a0617c1c2
nilswg <nilsonweng@gmail.com>
2022-08-28 14:45:59 +0800

    p0070 更新註解

--- JS/DP/easy/p0070_climbingStairs.js
+++ JS/DP/easy/p0070_climbingStairs.js
@@ -38,25 +38,26 @@
 // 2.改採用 DP 優化時間複雜度
 // 3.BottomUp 優化空間複雜度
 
-// Solution1 : 暴力解
-//
-//                    f(5)
-//              /             \
-//           f(4)              f(3)
-//         /      \           /   \
-//      f(3)     f(2)       f(2)   f(1)
-//      /  \     /   \      /   \
-//    f(2) f(1) f(1) f(0) f(1)  f(0)
-//    /  \
-//  f(1) f(0)
-//
-// 沒有任何優化前，其決策樹結構接近二元樹，Time Complexity: O(2^N)
-//
-// 複雜度
-// Time Complexity : O(2^N)
-// Space Complexity: O(N) ； N 即決策樹的高度
-
 /**
+ * Solution1 : 暴力解
+ *
+ *                f(4)
+ *             /         \
+ *          /               \
+ *        f(3)             f(2)
+ *       /    \          /     \
+ *    f(2)    f(1)     f(1)    f(0)
+ *    /  \    /  \     /  \    /  \
+ *  f(1)f(0) ∅    ∅   ∅   ∅   ∅   ∅
+ *
+ * 其中， f(1)、f(0) 就返回 1，但是為了方便分析，使用 ∅ 表示空節點
+ * 無DP優化前，遞迴樹結構接近二元樹，時間複雜度就是其樹的節點總數量2^N-1
+ * 即Time Complexity: O(2^N)。
+ *
+ * 複雜度
+ * Time Complexity : O(2^N)
+ * Space Complexity: O(N) ； N 即遞迴樹的高度
+ *
  * @param {number} n
  * @return {number}
  */
@@ -71,53 +72,62 @@ var climbStairsBrute = function (n) {
   return helper(n);
 };
 
-// Solution2 : 使用 DP
-//
-//                dp[5]
-//              /       \
-//            dp[4]     dp[3](可重用)
-//          /      \
-//        dp[3]    dp[2](可重用)
-//       /   \
-//    dp[2]  dp[1](可重用)
-//    /   \
-//  dp[1]  dp[0]
-//
-// 將相同節點進行保存後，其決策樹結構為單臂的左子樹，其深度為 N，Time Complexity: O(N)
-//
-// 複雜度
-// Time Complexity : O(N)
-// Space Complexity: O(N) ； N 即決策樹的高度
-
 /**
+ * Solution2 : 使用 DP Top-Down
+ *
+ * DP : [1, 1, 2, 3, 5]
+ *
+ *                dp[5]
+ *              /       \
+ *            dp[4]     dp[3](可重用)
+ *          /      \
+ *        dp[3]    dp[2](可重用)
+ *       /   \
+ *    dp[2]  dp[1](可重用)
+ *    /   \
+ *  dp[1]  dp[0]
+ *
+ * 將相同節點進行保存後，其遞迴樹結構為單臂的左子樹，其深度為 N，Time Complexity: O(N)
+ *
+ * 複雜度
+ * Time Complexity : O(N)
+ * Space Complexity: O(N) ； N 即遞迴樹的高度
+ *
  * @param {number} n
  * @return {number}
+ *
  */
 var climbStairsDP = function (n) {
   if (n < 1) return 0;
-  let dp = [];
+  let dp = [1, 1]; //if (n < 2) return 1;
+
   let helper = (n) => {
-    if (n < 2) return 1;
     if (dp[n]) return dp[n];
-    dp[n - 1] = helper(n - 1);
-    dp[n - 2] = helper(n - 2);
-    return dp[n - 1] + dp[n - 2];
+    dp[n] = helper(n - 1) + helper(n - 2);
+    return dp[n];
   };
 
   return helper(n);
 };
 
-// Solution3 : 使用 BottomUp
-//
-// f(0) + f(1) -> f(2) -> f(3) -> f(4) -> f(5)
-//
-// 採用 BottomUp 優化儲存空間， Space Complexity: O(1)。
-//
-// 複雜度
-// Time Complexity : O(N)
-// Space Complexity: O(1)
-
 /**
+ * Solution3 : 使用 BottomUp
+ *
+ * f(0) + f(1) -> f(2) -> f(3) -> f(4) -> f(5)
+ *
+ *         ta    a      b
+ *               1(n=1) 1(n=0)
+ * n=2      1    2      1
+ * n=3      2    3      2
+ * n=4      3    5      3
+ * n=5      5    8      5
+ *
+ * 採用 BottomUp 優化儲存空間， Space Complexity: O(1)。
+ *
+ * 複雜度
+ * Time Complexity : O(N)
+ * Space Complexity: O(1)
+ *
  * @param {number} n
  * @return {number}
  */
@@ -137,7 +147,7 @@ var climbStairsBottomUp = function (n) {
 
 // 測試
 (function () {
-  console.log('Testing [p0070_climbingStairs]...');
+  console.log("Testing [p0070_climbingStairs]...");
 
   const testingWith = (cb) => {
     console.log(cb(0) === 0);
@@ -148,14 +158,14 @@ var climbStairsBottomUp = function (n) {
     console.log(cb(8) === 34);
   };
 
-  console.log('Testing [climbStairsBrute]...');
+  console.log("Testing [climbStairsBrute]...");
   testingWith(climbStairsBrute);
 
-  console.log('Testing [climbStairsDP]...');
+  console.log("Testing [climbStairsDP]...");
   testingWith(climbStairsDP);
 
-  console.log('Testing [climbStairsBottomUp]...');
+  console.log("Testing [climbStairsBottomUp]...");
   testingWith(climbStairsBottomUp);
 
-  console.log('All Testing Passed ✅');
+  console.log("All Testing Passed ✅");
 })();
